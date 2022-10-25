0b0ac7ac405cfe69fb1c0db115bff2ef0b63e9cf
CarlosViniciusBizinottoPrates <viniciusbizinotto@gmail.com>
2022-10-24 22:57:10 -0300

    Validacoes

--- JavaBead2/src/App.java
+++ JavaBead2/src/App.java
@@ -1,5 +1,44 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        
+        int i1 =  30;
+        int i2 = 60;
+        float f1 = 3.6f;
+        float f2 = 6.3f;
+        double d1 = 69.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Carlos";
+        String s2 = "Vinicius";
+        String s3 = "Prates";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1212L;
+        Long l2 = 1414L;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("i1 == i2 " + (i1 == i2));
+        System.out.println("i1 != i2 " + (i1 != i2));
+        System.out.println("i1 > i2 " + (i1 > i2));
+        System.out.println("i1 <= i2 " + (i1 <= i2));
+
+        System.out.println("f1 == f2 " + (f1 == f2));
+        System.out.println("f1 != f2 " + (f1 != f2));
+        System.out.println("f1 > f2 " + (f1 > f2));
+        System.out.println("f1 <= f2 " + (f1 <= f2));
+
+        System.out.println("c1 == c2 " + (c1 == c2));
+        System.out.println("c1 != c2 " + (c1 != c2));
+        System.out.println("c1 > c2 " + (c1 > c2));
+        System.out.println("c1 <= c2 " + (c1 <= c2));
+
+        System.out.println("s1 == s2 " + (s1 == s2));
+        System.out.println("s1 != s2 " + (s1 != s2));
+
+
+
+
     }
 }
